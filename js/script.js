70f3e6f9f6df6aa8b2974432f9c08b6c8e6bbdf2
Ra11y <roman.alt@access-for-all.ch>
2025-12-04 17:26:25 +0100

    styled sub-views
    group folders in the view folder are now expandable buttons in the navigation. This works for multiple levels. The currently active page is not yet displayed and does not work with expanded levels yet.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 // elements need to be declared via id
 function addAsCode(sourceElement, language) {
-    let sourceString = document.getElementById(sourceElement)
-    sourceString.removeAttribute('id')
-    document.currentScript.parentNode.append(sourceString.outerHTML)
-    document.currentScript.parentNode.getElementsByTagName("script")[0].remove()
-    //document.currentScript.parentNode.classList.add("code-snippet")
-    //hljs.highlightAll()
+  let sourceString = document.getElementById(sourceElement);
+  sourceString.removeAttribute("id");
+  document.currentScript.parentNode.append(sourceString.outerHTML);
+  document.currentScript.parentNode.getElementsByTagName("script")[0].remove();
+  //document.currentScript.parentNode.classList.add("code-snippet")
+  //hljs.highlightAll()
 }
 
 function toggle() {
-    document.body.classList.toggle("light");
+  document.body.classList.toggle("light");
+}
+
+function toggleTree(btn) {
+  const group = btn.parentElement.querySelector(".tree-children");
+  const isOpen = btn.getAttribute("aria-expanded") === "true";
+
+  btn.setAttribute("aria-expanded", !isOpen);
+  btn.querySelector(".tree-level-toggle-indicator").textContent = isOpen
+    ? "─"
+    : "|";
+  group.classList.toggle("hidden", isOpen);
 }
